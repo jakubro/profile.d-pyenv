71778edf3d79c1f9fb874e8a3aa4923f41435638
Jakub Roman <jakub@jakubroman.sk>
2023-11-27 13:16:30 +0100

    error handling

--- bin/install.sh
+++ bin/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -eo pipefail
 
 if [ ! -d ~/.pyenv ]; then
 
   log_info "Installing pyenv..."
 
-  curl -s https://pyenv.run | bash
+  curl -s https://pyenv.run | bash || exit 1
+
+  log_info "You can safely ignore any instructions printed above"
+
+else
+
+  log_info "Updating pyenv..."
+
+  cd ~/.pyenv || exit 1
+  git pull || exit 1
 
 fi
-
-log_info "Updating pyenv..."
-
-cd ~/.pyenv
-git pull
-
-log_info "You can safely ignore any instructions printed above"
